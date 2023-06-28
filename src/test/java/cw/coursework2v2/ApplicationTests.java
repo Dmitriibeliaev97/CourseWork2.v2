bc3028bc6cc6a1366bb007d10b40f5528e7b1f21
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-06-28 23:48:32 +0300

    Initial commit

---
+++ src/test/java/cw/coursework2v2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cw.coursework2v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
